db8cee726d4f8b1530b53a76f524b7d9a73461e7
Alexander Kiselev <pumad456@gmail.com>
2025-12-19 19:12:13 +0100

    fix: исправлен  move-image

--- backend/src/move-image.ts
+++ backend/src/move-image.ts
@@ -1,19 +1,17 @@
 import path from 'path';
-import fs from 'fs/promises';
+import fs from 'fs';
+import fsPromises from 'fs/promises';
 import config from './config';
-import BadRequestError from './errors/bad-request-error';
 
 export const moveImageFromTemp = async (fileName: string) => {
-  const base = path.basename(fileName); // "abc.png"
+  const base = path.basename(fileName);
 
   const from = path.join(process.cwd(), 'public', config.UPLOAD_PATH_TEMP, base);
   const to = path.join(process.cwd(), 'public', config.UPLOAD_PATH, base);
 
-  try {
-    await fs.rename(from, to);
-  } catch {
-    throw new BadRequestError('Файл изображения не найден во временной папке');
-  }
+  if (!fs.existsSync(from)) return;
+
+  await fsPromises.rename(from, to);
 };
 
 export default moveImageFromTemp;
